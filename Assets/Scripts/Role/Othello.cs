c39a9eff6e241b6c6c7cdf1f9a6b0b4dc7bf6709
SAKURA-778 <poken.kyuremu100@gmail.com>
2025-06-27 22:23:48 +0900

    役職オセロの実装

--- Assets/Scripts/Role/Othello.cs
+++ Assets/Scripts/Role/Othello.cs
@@ -33,14 +33,6 @@ class  : MonoRole
             BoardInfo.GetCell(x - 4, y) == my)
             return true;
         //縦
-        //上
-        if (y < 6 &&
-            BoardInfo.GetCell(x, y + 1) == opo &&
-            BoardInfo.GetCell(x, y + 2) == opo &&
-            BoardInfo.GetCell(x, y + 3) == opo &&
-            BoardInfo.GetCell(x, y + 4) == my)
-            return true;
-        //下
         if (y > 3 &&
             BoardInfo.GetCell(x, y - 1) == opo &&
             BoardInfo.GetCell(x, y - 2) == opo &&
@@ -63,7 +55,6 @@ class  : MonoRole
             BoardInfo.GetCell(x + 4, y + 4) == my)
             return true;
         //バックスラッシュ
-
         //右下
         if (x > 3 && y < 6 &&
             BoardInfo.GetCell(x - 1, y + 1) == opo &&
@@ -83,6 +74,67 @@ class  : MonoRole
 
     public override bool WinningConditionOpoTurn()
     {
-        return base.WinningConditionOpuTurn();
+        int x = BoardInfo.GetLastX();
+        int y = BoardInfo.GetLastY();
+        int opo = BoardInfo.GetCell(x, y);
+        int my = BoardInfo.GetCell(BoardInfo.GetPreLastX(), BoardInfo.GetPreLastY());
+        // 横
+        if (x >= 3 && x <= 8)
+            if (my == BoardInfo.GetCell(x + 1, y) &&
+                opo == BoardInfo.GetCell(x - 1, y) &&
+                opo == BoardInfo.GetCell(x - 2, y) &&
+                my == BoardInfo.GetCell(x - 3, y))
+                return true;
+        if (x >= 2 && x <= 7)
+            if (my == BoardInfo.GetCell(x + 2, y) &&
+                opo == BoardInfo.GetCell(x + 1, y) &&
+                opo == BoardInfo.GetCell(x - 1, y) &&
+                my == BoardInfo.GetCell(x - 2, y))
+                return true;
+        if (x >= 1 && x <= 6)
+            if (my == BoardInfo.GetCell(x + 3, y) &&
+                opo == BoardInfo.GetCell(x + 2, y) &&
+                opo == BoardInfo.GetCell(x + 1, y) &&
+                my == BoardInfo.GetCell(x - 1, y))
+                return true;
+        // スラッシュ
+        if (x >= 3 && x <= 8 && y >= 3 && y <= 8)
+            if (my == BoardInfo.GetCell(x + 1, y + 1) &&
+                opo == BoardInfo.GetCell(x - 1, y - 1) &&
+                opo == BoardInfo.GetCell(x - 2, y - 2) &&
+                my == BoardInfo.GetCell(x - 3, y - 3))
+                return true;
+        if (x >= 2 && x <= 7 && y >= 2 && y <= 7)
+            if (my == BoardInfo.GetCell(x + 2, y + 2) &&
+                opo == BoardInfo.GetCell(x + 1, y + 1) &&
+                opo == BoardInfo.GetCell(x - 1, y - 1) &&
+                my == BoardInfo.GetCell(x - 2, y - 2))
+                return true;
+        if (x >= 1 && x <= 6 && y >= 1 && y <= 6)
+            if (my == BoardInfo.GetCell(x + 3, y + 3) &&
+                opo == BoardInfo.GetCell(x + 2, y + 2) &&
+                opo == BoardInfo.GetCell(x + 1, y + 1) &&
+                my == BoardInfo.GetCell(x - 1, y - 1))
+                return true;
+        // バックスラッシュ
+        if (x >= 3 && x <= 8 && y >= 1 && y <= 6)
+            if (my == BoardInfo.GetCell(x + 1, y - 1) &&
+                opo == BoardInfo.GetCell(x - 1, y + 1) &&
+                opo == BoardInfo.GetCell(x - 2, y + 2) &&
+                my == BoardInfo.GetCell(x - 3, y + 3))
+                return true;
+        if (x >= 2 && x <= 7 && y >= 2 && y <= 7)
+            if (my == BoardInfo.GetCell(x + 2, y - 2) &&
+                opo == BoardInfo.GetCell(x + 1, y - 1) &&
+                opo == BoardInfo.GetCell(x - 1, y + 1) &&
+                my == BoardInfo.GetCell(x - 2, y + 2))
+                return true;
+        if (x >= 1 && x <= 6 && y >= 3 && y <= 8)
+            if (my == BoardInfo.GetCell(x + 3, y - 3) &&
+                opo == BoardInfo.GetCell(x + 2, y - 2) &&
+                opo == BoardInfo.GetCell(x + 1, y - 1) &&
+                my == BoardInfo.GetCell(x - 1, y + 1))
+                return true;
+        return false;
     }
 }
