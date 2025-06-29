b758fa094c3201a3ddc2555517fe5e31727469c1
SAKURA-778 <poken.kyuremu100@gmail.com>
2025-06-29 22:29:59 +0900

    役職クラスびっくり箱の実装

--- Assets/Scripts/Role/SurpriseBox.cs
+++ Assets/Scripts/Role/SurpriseBox.cs
@@ -93,20 +93,36 @@ class SurpriseBox : MonoRole
             BoardInfo.GetCell(x + 3, y + 3) == my)
             count++;
         //バックスラッシュ
-        //右下
-        if (x > 3 && y < 6 &&
+        if (x >= 3 && y <= 6 &&
             BoardInfo.GetCell(x - 1, y + 1) == opo &&
+            BoardInfo.GetCell(x - 2, y + 2) == my &&
+            BoardInfo.GetCell(x - 3, y + 3) == my)
+            count++;
+        if (x >= 3 && y <= 6 &&
+            BoardInfo.GetCell(x - 1, y + 1) == my &&
             BoardInfo.GetCell(x - 2, y + 2) == opo &&
-            BoardInfo.GetCell(x - 3, y + 3) == opo &&
-            BoardInfo.GetCell(x - 4, y + 4) == my)
-            return true;
-        //左上
-        if (x < 6 && y > 3 &&
+            BoardInfo.GetCell(x - 3, y + 3) == my)
+            count++;
+        if (x >= 2 && y <= 7 && x <= 8 && y >= 1 &&
+            BoardInfo.GetCell(x + 1, y - 1) == my &&
+            BoardInfo.GetCell(x - 1, y + 1) == opo &&
+            BoardInfo.GetCell(x - 2, y + 2) == my)
+            count++;
+        if (x >= 1 && y <= 8 && x <= 7 && y >= 2 &&
+            BoardInfo.GetCell(x + 2, y - 2) == my &&
             BoardInfo.GetCell(x + 1, y - 1) == opo &&
+            BoardInfo.GetCell(x - 1, y + 1) == my)
+            count++;
+        if (x <= 6 && y => 3 &&
+            BoardInfo.GetCell(x + 1, y - 1) == opo &&
+            BoardInfo.GetCell(x + 2, y - 2) == my &&
+            BoardInfo.GetCell(x + 3, y - 3) == my)
+            count++;
+        if (x <= 6 && y => 3 &&
+            BoardInfo.GetCell(x + 1, y - 1) == my &&
             BoardInfo.GetCell(x + 2, y - 2) == opo &&
-            BoardInfo.GetCell(x + 3, y - 3) == opo &&
-            BoardInfo.GetCell(x + 4, y - 4) == my)
-            return true;
+            BoardInfo.GetCell(x + 3, y - 3) == my)
+            count++;
         if (count >= 3) return true;
         return false;
     }
@@ -118,62 +134,39 @@ class SurpriseBox : MonoRole
         int opo = BoardInfo.GetCell(x, y);
         int my = BoardInfo.GetCell(BoardInfo.GetPreLastX(), BoardInfo.GetPreLastY());
         // 横
-        if (x >= 3 && x <= 8)
-            if (my == BoardInfo.GetCell(x + 1, y) &&
-                opo == BoardInfo.GetCell(x - 1, y) &&
-                opo == BoardInfo.GetCell(x - 2, y) &&
-                my == BoardInfo.GetCell(x - 3, y))
-                return true;
-        if (x >= 2 && x <= 7)
-            if (my == BoardInfo.GetCell(x + 2, y) &&
-                opo == BoardInfo.GetCell(x + 1, y) &&
-                opo == BoardInfo.GetCell(x - 1, y) &&
-                my == BoardInfo.GetCell(x - 2, y))
-                return true;
-        if (x >= 1 && x <= 6)
-            if (my == BoardInfo.GetCell(x + 3, y) &&
-                opo == BoardInfo.GetCell(x + 2, y) &&
-                opo == BoardInfo.GetCell(x + 1, y) &&
-                my == BoardInfo.GetCell(x - 1, y))
-                return true;
+        if (x >= 1 && x <= 7 &&
+            my == BoardInfo.GetCell(x + 2, y) &&
+            my == BoardInfo.GetCell(x + 1, y) &&
+            my == BoardInfo.GetCell(x - 1, y))
+            count++;
+        if (x >= 2 && x <= 8 &&
+            my == BoardInfo.GetCell(x + 1, y) &&
+            my == BoardInfo.GetCell(x - 1, y) &&
+            my == BoardInfo.GetCell(x - 2, y))
+            count++;
         // スラッシュ
-        if (x >= 3 && x <= 8 && y >= 3 && y <= 8)
-            if (my == BoardInfo.GetCell(x + 1, y + 1) &&
-                opo == BoardInfo.GetCell(x - 1, y - 1) &&
-                opo == BoardInfo.GetCell(x - 2, y - 2) &&
-                my == BoardInfo.GetCell(x - 3, y - 3))
-                return true;
-        if (x >= 2 && x <= 7 && y >= 2 && y <= 7)
-            if (my == BoardInfo.GetCell(x + 2, y + 2) &&
-                opo == BoardInfo.GetCell(x + 1, y + 1) &&
-                opo == BoardInfo.GetCell(x - 1, y - 1) &&
-                my == BoardInfo.GetCell(x - 2, y - 2))
-                return true;
-        if (x >= 1 && x <= 6 && y >= 1 && y <= 6)
-            if (my == BoardInfo.GetCell(x + 3, y + 3) &&
-                opo == BoardInfo.GetCell(x + 2, y + 2) &&
-                opo == BoardInfo.GetCell(x + 1, y + 1) &&
-                my == BoardInfo.GetCell(x - 1, y - 1))
-                return true;
+        if (x >= 2 && x <= 8 && y >= 2 && y <= 8 &&
+            my == BoardInfo.GetCell(x + 1, y + 1) &&
+            my == BoardInfo.GetCell(x - 1, y - 1) &&
+            my == BoardInfo.GetCell(x - 2, y - 2))
+            count++;
+        if (x >= 1 && x <= 7 && y >= 1 && y <= 7 &&
+            my == BoardInfo.GetCell(x + 2, y + 2) &&
+            my == BoardInfo.GetCell(x + 1, y + 1) &&
+            my == BoardInfo.GetCell(x - 1, y - 1))
+            count++;
         // バックスラッシュ
-        if (x >= 3 && x <= 8 && y >= 1 && y <= 6)
-            if (my == BoardInfo.GetCell(x + 1, y - 1) &&
-                opo == BoardInfo.GetCell(x - 1, y + 1) &&
-                opo == BoardInfo.GetCell(x - 2, y + 2) &&
-                my == BoardInfo.GetCell(x - 3, y + 3))
-                return true;
-        if (x >= 2 && x <= 7 && y >= 2 && y <= 7)
-            if (my == BoardInfo.GetCell(x + 2, y - 2) &&
-                opo == BoardInfo.GetCell(x + 1, y - 1) &&
-                opo == BoardInfo.GetCell(x - 1, y + 1) &&
-                my == BoardInfo.GetCell(x - 2, y + 2))
-                return true;
-        if (x >= 1 && x <= 6 && y >= 3 && y <= 8)
-            if (my == BoardInfo.GetCell(x + 3, y - 3) &&
-                opo == BoardInfo.GetCell(x + 2, y - 2) &&
-                opo == BoardInfo.GetCell(x + 1, y - 1) &&
-                my == BoardInfo.GetCell(x - 1, y + 1))
-                return true;
+        if (x >= 2 && x <= 8 && y >= 1 && y <= 7 &&
+            my == BoardInfo.GetCell(x + 1, y - 1) &&
+            my == BoardInfo.GetCell(x - 1, y + 1) &&
+            my == BoardInfo.GetCell(x - 2, y + 2))
+            count++;
+        if (x >= 1 && x <= 7 && y >= 2 && y <= 8 &&
+            my == BoardInfo.GetCell(x + 2, y - 2) &&
+            my == BoardInfo.GetCell(x + 1, y - 1) &&
+            my == BoardInfo.GetCell(x - 1, y + 1))
+            count++;
+        if (count >= 3) return true;
         return false;
     }
 }
